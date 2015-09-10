84f77276ee75b427b84eaacceaf5caf362d2ecee
Vadim Shashenko <v.shashenko@gmail.com>
2015-09-10 16:02:36 +0200

    Refactoring: added IDEA project component and moved some code there.

---
+++ src/gtestrunner/GoogleTestRunner.java
@@ -0,0 +1,159 @@
+package gtestrunner;
+
+import com.intellij.openapi.application.ApplicationInfo;
+import com.intellij.openapi.components.ProjectComponent;
+import com.intellij.openapi.fileEditor.OpenFileDescriptor;
+import com.intellij.openapi.project.Project;
+import com.intellij.openapi.vfs.*;
+import org.jetbrains.annotations.NotNull;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class GoogleTestRunner implements ProjectComponent
+{
+    private Project _project;
+    private ExecutableInfo _exeInfo;
+    private Exception _exeInfoError;
+    private IProjectListener _projectListener;
+    private VirtualFileListener _virtualFileListener;
+
+    public GoogleTestRunner(Project project)
+    {
+        _project = project;
+    }
+
+    public void initComponent()
+    {
+        // TODO: insert component initialization logic here
+    }
+
+    public void disposeComponent()
+    {
+        // TODO: insert component disposal logic here
+    }
+
+    @NotNull
+    public String getComponentName()
+    {
+        return "GoogleTestRunner";
+    }
+
+    public void projectOpened()
+    {
+        updateExeInfo();
+
+        _virtualFileListener = new VirtualFileAdapter()
+        {
+            @Override
+            public void contentsChanged(@NotNull VirtualFileEvent event)
+            {
+                onFileContentsChanged(event);
+            }
+        };
+
+        VirtualFileSystem virtualFileSystem = _project.getBaseDir().getFileSystem();
+        virtualFileSystem.addVirtualFileListener(_virtualFileListener);
+    }
+
+    public void projectClosed()
+    {
+        VirtualFileSystem virtualFileSystem = _project.getBaseDir().getFileSystem();
+        virtualFileSystem.removeVirtualFileListener(_virtualFileListener);
+        _virtualFileListener = null;
+    }
+
+    public void set_projectListener(IProjectListener projectListener)
+    {
+        _projectListener = projectListener;
+    }
+
+    public ExecutableInfo get_exeInfo() throws Exception
+    {
+        if (_exeInfo == null)
+        {
+            throw _exeInfoError;
+        }
+
+        return _exeInfo;
+    }
+
+    public void openTestCase(String caseName) throws Exception
+    {
+        ProcessResult pr = Utils.executeProcess(null, "grep", "-nwrI", "--include=*.cpp", caseName, _project.getBaseDir().getPath());
+
+        if (pr.outputLines.size() > 0)
+        {
+            String line = pr.outputLines.get(0);
+            String[] parts = line.split(":");
+            String filePath = parts[0];
+            int lineNumber = Integer.parseInt(parts[1]) - 1;
+
+            openFile(filePath, lineNumber);
+        }
+    }
+
+    public void openFile(String filePath, int lineNumber) throws Exception
+    {
+        VirtualFileSystem virtualFileSystem = _project.getBaseDir().getFileSystem();
+
+        VirtualFile file = virtualFileSystem.findFileByPath(filePath);
+        OpenFileDescriptor fileDescriptor = new OpenFileDescriptor(_project, file, lineNumber, 0);
+        fileDescriptor.navigateInEditor(_project, true);
+    }
+
+    private void updateExeInfo()
+    {
+        try
+        {
+            _exeInfo = readExecutablePath(_project);
+        }
+        catch (Exception ex)
+        {
+            _exeInfoError = ex;
+        }
+
+        if (_projectListener != null)
+        {
+            _projectListener.exeInfoChanged();
+        }
+    }
+
+    private static ExecutableInfo readExecutablePath(@NotNull Project project) throws Exception
+    {
+        String workspacePath = project.getWorkspaceFile().getPath();
+        TargetInfo targetInfo = CLionProjectReader.readSelectedTarget(workspacePath);
+
+        ApplicationInfo appInfo = ApplicationInfo.getInstance();
+        String clionFolder = String.format(".clion%s%s", appInfo.getMajorVersion(), appInfo.getMinorVersion().split("\\.")[0]);
+
+        Path cbpPath = Paths.get(
+                System.getProperty("user.home"),
+                clionFolder,
+                "system",
+                "cmake",
+                "generated",
+                project.getLocationHash(),
+                project.getLocationHash(),
+                targetInfo.config,
+                targetInfo.projectName + ".cbp");
+
+        String exePath = CLionProjectReader.readExecutablePath(cbpPath.toString(), targetInfo.runName);
+
+        ExecutableInfo exeInfo = new ExecutableInfo();
+        exeInfo.command = exePath;
+        exeInfo.arguments = targetInfo.params;
+
+        return exeInfo;
+    }
+
+    private void onFileContentsChanged(VirtualFileEvent event)
+    {
+        String workspacePath = _project.getWorkspaceFile().getPath();
+
+        if (event.getFile().getPath().equals(workspacePath))
+        {
+            updateExeInfo();
+        }
+    }
+}
--- src/gtestrunner/RunnerToolWindowFactory.java
+++ src/gtestrunner/RunnerToolWindowFactory.java
@@ -1,12 +1,6 @@
 package gtestrunner;
 
-import com.intellij.notification.Notification;
-import com.intellij.notification.NotificationType;
-import com.intellij.notification.Notifications;
-import com.intellij.openapi.application.ApplicationInfo;
-import com.intellij.openapi.fileEditor.OpenFileDescriptor;
 import com.intellij.openapi.project.*;
-import com.intellij.openapi.vfs.*;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
 import com.intellij.ui.content.Content;
@@ -16,40 +10,28 @@ import org.jetbrains.annotations.NotNull;
 import javax.swing.*;
 import java.io.PrintWriter;
 import java.io.StringWriter;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 
 public class RunnerToolWindowFactory implements ToolWindowFactory, DumbAware
 {
     private RunnerToolWindow _runnerToolWindow;
     private ConsoleOutputWindow _consoleWindow;
-    private Project _project;
-    private VirtualFileListener _virtualFileListener;
 
     public RunnerToolWindowFactory()
     {
-        Notifications.Bus.notify(new Notification("test", "RunnerToolWindowFactory", "a", NotificationType.INFORMATION));
-
-        ProjectManager.getInstance().addProjectManagerListener(new ProjectManagerAdapter()
-        {
-            @Override
-            public void projectOpened(Project project)
-            {
-                onProjectOpened(project);
-            }
-
-            @Override
-            public void projectClosed(Project project)
-            {
-                onProjectClosed(project);
-            }
-        });
     }
 
     @Override
-    public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow)
+    public void createToolWindowContent(@NotNull final Project project, @NotNull ToolWindow toolWindow)
     {
-        Notifications.Bus.notify(new Notification("test", "createToolWindowContent", "a", NotificationType.INFORMATION));
+        final GoogleTestRunner runner = project.getComponent(GoogleTestRunner.class);
+        runner.set_projectListener(new IProjectListener()
+        {
+            @Override
+            public void exeInfoChanged()
+            {
+                updateExecutablePath(project);
+            }
+        });
 
         _runnerToolWindow = new RunnerToolWindow();
         _consoleWindow = new ConsoleOutputWindow();
@@ -60,13 +42,13 @@ public class RunnerToolWindowFactory implements ToolWindowFactory, DumbAware
             @Override
             public void gotoFile(String filePath, int lineNumber) throws Exception
             {
-                openFile(filePath, lineNumber);
+                runner.openFile(filePath, lineNumber);
             }
 
             @Override
             public void gotoTestCase(String caseName) throws Exception
             {
-                openTestCase(caseName);
+                runner.openTestCase(caseName);
             }
         });
 
@@ -77,87 +59,15 @@ public class RunnerToolWindowFactory implements ToolWindowFactory, DumbAware
 
         Content consoleContent = contentFactory.createContent(_consoleWindow.root, "Console", false);
         toolWindow.getContentManager().addContent(consoleContent);
+
+        updateExecutablePath(project);
     }
 
-    private void onProjectOpened(Project project)
-    {
-        Notifications.Bus.notify(new Notification("test", "projectOpened " + project.getName(), "a", NotificationType.INFORMATION));
-
-        _project = project;
-
-        updateExecutablePath();
-
-        _virtualFileListener = new VirtualFileAdapter()
-        {
-            @Override
-            public void contentsChanged(@NotNull VirtualFileEvent event)
-            {
-                onFileContentsChanged(event);
-            }
-        };
-
-        VirtualFileSystem virtualFileSystem = project.getBaseDir().getFileSystem();
-        virtualFileSystem.addVirtualFileListener(_virtualFileListener);
-    }
-
-    private void onProjectClosed(Project project)
-    {
-        Notifications.Bus.notify(new Notification("test", "projectClosed ", "a", NotificationType.INFORMATION));
-
-        VirtualFileSystem virtualFileSystem = project.getBaseDir().getFileSystem();
-        virtualFileSystem.removeVirtualFileListener(_virtualFileListener);
-
-        _project = null;
-    }
-
-    private static ExecutableInfo readExecutablePath(@NotNull Project project) throws Exception
-    {
-        String workspacePath = project.getWorkspaceFile().getPath();
-        TargetInfo targetInfo = CLionProjectReader.readSelectedTarget(workspacePath);
-
-        ApplicationInfo appInfo = ApplicationInfo.getInstance();
-        String clionFolder = String.format(".clion%s%s", appInfo.getMajorVersion(), appInfo.getMinorVersion().split("\\.")[0]);
-
-        Path cbpPath = Paths.get(
-                System.getProperty("user.home"),
-                clionFolder,
-                "system",
-                "cmake",
-                "generated",
-                project.getLocationHash(),
-                project.getLocationHash(),
-                targetInfo.config,
-                targetInfo.projectName + ".cbp");
-
-        String exePath = CLionProjectReader.readExecutablePath(cbpPath.toString(), targetInfo.runName);
-
-        ExecutableInfo exeInfo = new ExecutableInfo();
-        exeInfo.command = exePath;
-        exeInfo.arguments = targetInfo.params;
-
-        return exeInfo;
-    }
-
-    private void onFileContentsChanged(VirtualFileEvent event)
-    {
-        if (!_project.isOpen())
-        {
-            return;
-        }
-
-        String workspacePath = _project.getWorkspaceFile().getPath();
-
-        if (event.getFile().getPath().equals(workspacePath))
-        {
-            updateExecutablePath();
-        }
-    }
-
-    private void updateExecutablePath()
+    private void updateExecutablePath(Project project)
     {
         try
         {
-            ExecutableInfo exeInfo = readExecutablePath(_project);
+            ExecutableInfo exeInfo = project.getComponent(GoogleTestRunner.class).get_exeInfo();
             _runnerToolWindow.setExecutablePath(exeInfo.command, exeInfo.arguments);
         }
         catch (Exception ex)
@@ -167,29 +77,5 @@ public class RunnerToolWindowFactory implements ToolWindowFactory, DumbAware
 
             JOptionPane.showMessageDialog(null, sw.toString());
         }
-    }
-
-    private void openTestCase(String caseName) throws Exception
-    {
-        ProcessResult pr = Utils.executeProcess(null, "grep", "-nwrI", "--include=*.cpp", caseName, _project.getBaseDir().getPath());
-
-        if (pr.outputLines.size() > 0)
-        {
-            String line = pr.outputLines.get(0);
-            String[] parts = line.split(":");
-            String filePath = parts[0];
-            int lineNumber = Integer.parseInt(parts[1]) - 1;
-
-            openFile(filePath, lineNumber);
-        }
-    }
-
-    private void openFile(String filePath, int lineNumber) throws Exception
-    {
-        VirtualFileSystem virtualFileSystem = _project.getBaseDir().getFileSystem();
-
-        VirtualFile file = virtualFileSystem.findFileByPath(filePath);
-        OpenFileDescriptor fileDescriptor = new OpenFileDescriptor(_project, file, lineNumber, 0);
-        fileDescriptor.navigateInEditor(_project, true);
     }
 }
